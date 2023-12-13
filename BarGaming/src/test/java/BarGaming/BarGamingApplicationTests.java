8d64d3c6aac42f54dee710060a6f595deafb5b28
issa <issa.g.299@gmail.com>
2023-12-13 17:04:30 +0100

    debut projet

---
+++ BarGaming/src/test/java/BarGaming/BarGamingApplicationTests.java
@@ -0,0 +1,13 @@
+package BarGaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarGamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
