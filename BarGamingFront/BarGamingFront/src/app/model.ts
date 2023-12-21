f96938178db5431f2f677e4810579041b8069799
Coco24100 <gaillarco@hotmail.fr>
2023-12-21 16:36:30 +0100

    Component client

--- BarGamingFront/BarGamingFront/src/app/model.ts
+++ BarGamingFront/BarGamingFront/src/app/model.ts
@@ -1,3 +1,7 @@
 export class Emprunt {
     constructor(public id?: number, public dateEmprunt?: string, public client?: string, public jeuxSociete?: string){}
 }
+
+export class Client {
+    constructor(public id?: number, public nom?: string, public prenom?: string) {}
+}
