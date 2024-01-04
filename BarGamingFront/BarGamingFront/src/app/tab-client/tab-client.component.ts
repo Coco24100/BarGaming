76f58e7ed8635cb48ea461046596d850694f5600
issa <issa.g.299@gmail.com>
2024-01-04 10:31:14 +0100

    envoie email front

--- BarGamingFront/BarGamingFront/src/app/tab-client/tab-client.component.ts
+++ BarGamingFront/BarGamingFront/src/app/tab-client/tab-client.component.ts
@@ -14,85 +14,85 @@ export class TabClientComponent {
 
   constructor(private clientService: ClientService, private router: Router, private eventService: EvenementService, private authService: AuthService) {
 
-}
-activeClass: string = 'header-info';
-tabInfos?:Client = undefined;
-tabLocation: boolean = false;
-tabInscription: boolean = false;
-tablHistorique: boolean=false;
+  }
+  activeClass: string = 'header-info';
+  tabInfos?: Client = undefined;
+  tabLocation: boolean = false;
+  tabInscription: boolean = false;
+  tablHistorique: boolean = false;
 
-showInfos(){
- this.tabInfos = new Client(); 
- this.tabLocation = false;
+  showInfos() {
+    this.tabInfos = new Client();
+    this.tabLocation = false;
+    this.tablHistorique = false;
+    this.tabInscription = false;
+    this.activeClass = 'header-info';
+    this.tabInfos = this.authService.getCompte();
+  }
+
+
+
+showInscription() {
+  this.tabInscription = true;
+  this.tabLocation = false;
   this.tablHistorique = false;
-  this.tabInscription = false;
-  this.activeClass = 'header-info';
-  this.tabInfos = this.authService.getCompte();
+  this.tabInfos = undefined;
+  this.activeClass = 'header-location';
 }
 
+showLocation() {
+  this.tabLocation = true;
+  this.tabInscription = false;
+  this.tablHistorique = false;
+  this.tabInfos = undefined;
+  this.activeClass = 'header-inscription';
+}
+
+showHistorique() {
+  this.tablHistorique = true;
+  this.tabLocation = false;
+  this.tabInscription = false;
+  this.tabInfos = undefined;
+  this.activeClass = 'header-historique';
+}
+
+save() {
+  if (this.tabInfos) {
+    this.clientService.update(this.tabInfos);
+  }
+  this.cancel();
+}
+
+cancel() {
+  this.tabInfos = undefined;
+}
+
+cloturer() {
+  if (confirm("Voulez vous vraiment supprimer votre compte ?")) {
+    this.authService.logout()
+    this.clientService.delete(this.tabInfos?.id)
+    alert("Votre Compte à bien été supprimé")
   }
 
-  showInscription() {
-    this.tabInscription = true;
-    this.tabLocation = false;
-    this.tablHistorique = false;
-    this.tabInfos = undefined;
-    this.activeClass = 'header-location';
-  }
 
-  showLocation() {
-    this.tabLocation = true;
-    this.tabInscription = false;
-    this.tablHistorique = false;
-    this.tabInfos = undefined;
-    this.activeClass = 'header-inscription';
-  }
+}
 
-  showHistorique() {
-    this.tablHistorique = true;
-    this.tabLocation = false;
-    this.tabInscription = false;
-    this.tabInfos = undefined;
-    this.activeClass = 'header-historique';
-  }
+voirTabJeuSociete() {
+  this.router.navigate(["/emprunt-jeu"]);
+}
 
-  save() {
-    if (this.tabInfos) {
-      this.clientService.update(this.tabInfos);
-    }
-    this.cancel();
-  }
+voirTabJeuVideo() {
+  this.router.navigate(["/emprunt-jeu"]);
 
-  cancel() {
-    this.tabInfos = undefined;
-  }
+}
 
-  cloturer() {
-    if (confirm("Voulez vous vraiment supprimer votre compte ?")) {
-      this.authService.logout()
-      this.clientService.delete(this.tabInfos?.id)
-      alert("Votre Compte à bien été supprimé")
-    }
+listEvents(): Evenement[] {
+  return this.eventService.findAll();
+}
 
-
-  }
-
-  voirTabJeuSociete() {
-    this.router.navigate(["/emprunt-jeu"]);
-  }
-
-  voirTabJeuVideo() {
-    this.router.navigate(["/emprunt-jeu"]);
-
-  }
-
-  listEvents(): Evenement[] {
-    return this.eventService.findAll();
-  }
-
-  inscription() {
-    this.router.navigate(["/inscription-evenement"]);
-  }
+inscription() {
+  this.router.navigate(["/inscription-evenement"]);
+}
 
 
 
